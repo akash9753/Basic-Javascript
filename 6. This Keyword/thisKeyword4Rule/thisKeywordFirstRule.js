34d557333e3604ae65903bbe0ec393ab0ddd7343
akash9753 <64675019+akash9753@users.noreply.github.com>
2023-07-04 03:01:54 +0530

    n

--- 6. This Keyword/thisKeyword4Rule/thisKeywordFirstRule.js
+++ 6. This Keyword/thisKeyword4Rule/thisKeywordFirstRule.js
@@ -6,7 +6,7 @@
 //when we call function using new keyword it does 4 things
 //1. it will create brand new object {}
 //2. new keyword will link this brand new object with another object which is the prototype of Vichle function by a __proto
-//3. new keyword ill call this vichle function with the value of this set to ne object
+//3. new keyword will call this vichle function with the value of this set to new object
 //Vehicle.call{}
 //4. if the Vehicle function is not returning anything it will return brand new object
 
---
+++ 7. call bind apply/sofiyaGoyal.js
@@ -0,0 +1,2 @@
+//call bind and apply are use to define the context of or change the context of this
+
