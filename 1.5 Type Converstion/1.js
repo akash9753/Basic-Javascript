ab378789a2863fde47f9aca9a239e738dd46da67
akash9753 <64675019+akash9753@users.noreply.github.com>
2023-07-09 17:59:57 +0530

    float

--- 1.5 Type Converstion/1.js
+++ 1.5 Type Converstion/1.js
@@ -1,8 +1,28 @@
 //Number to string
-
-
-
+let a = 10;
+let b2 = a.toString();
+// let b2 = a + '';
+console.log(b2);
+console.log(typeof b2);
+console.log("----------------------------------------------------")
+//String to nubber
 let a1 = "10"
+let a11 = parseInt(a1);
+console.log(a11);
+console.log(typeof a11);
+console.log("----------------------------------------------------")
 let a2 = "test"
+let a22 = parseInt(a2);
+console.log(a22);
+console.log(typeof a22);
+console.log("----------------------------------------------------")
 let a3 = "1test"
-
+let a23 = parseInt(a3);
+console.log(a23);
+console.log(typeof a23);
+console.log("----------------------------------------------------")
+let a4 = "12.12"
+let a24 = parseFloat(a4);
+console.log(a24);
+console.log(typeof a24);
+console.log("----------------------------------------------------")
