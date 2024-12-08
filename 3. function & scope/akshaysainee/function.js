682dbaec448e780db8932edcb8769db16680978f
akash patel <akash9753@gmail.com>
2024-12-08 17:02:33 +0530

    new

---
+++ 3. function & scope/akshaysainee/function.js
@@ -0,0 +1,47 @@
+//Function Statement //Function declaration
+function a(){
+    console.log("Function Statement");
+}
+a();
+
+//Function Expression
+var b = function(){
+    console.log("Function Expression");
+}
+b();
+
+//Anonymous function
+// function(){
+
+// }
+
+//Named function expression
+var z = function akash(params){
+    console.log("named function expression");
+}
+
+//Difference between parameter and argument
+var z = function akash(params){
+    console.log("named function expression");
+}
+z("args");
+
+//First class function
+//when function treated as value.passed as function as args,and return as function 
+
+//Arrow function 
+//es6 feature
+
+//1.syntax
+//2.does not have its own this. inherits from lexical
+//3.Does not have its own arguments object.
+//4.Can be used as a constructor
+//5.No, arrow functions cannot be hoisted in the same way as regular functions
+
+
+
+
+
+
+
+
